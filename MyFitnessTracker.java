b338a867a0fe92375c0fe53d40b1a4fa7fa74e41
Brynja Schultz <bes9992@nyu.edu>
2024-08-06 16:40:18 -0600

    testing first commit from brynja's laptop

--- MyFitnessTracker.java
+++ MyFitnessTracker.java
@@ -3,6 +3,9 @@
 public class MyFitnessTracker{
     public static void main (String args[]){
 
-        
+        System.out.print("TESTING!!!!");
+        ////COMMENTS
+        ///HELLO
+
     }
 }
