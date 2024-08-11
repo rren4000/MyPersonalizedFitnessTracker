3f04f7676e931ae43924c1868cef59be37809782
andaww <andaw820@gmail.com>
2024-08-11 03:06:14 -0400

    attempt at organizing BMR_Calc

--- BMR_Calc.java
+++ BMR_Calc.java
@@ -14,10 +14,10 @@ public class BMR_Calc {
     private int age;
     private int sex;
     private int goal_cal;
-
     private int goal_weight;
-    
-    // Constructor
+    private Scanner scanner = new Scanner(System.in);
+
+    // Constructor to initialize default values
     public BMR_Calc() {
         this.BMR = 0;
         this.weight = 0;
@@ -29,213 +29,127 @@ public class BMR_Calc {
         this.goal_weight = 0;
     }
 
-    public void close_BMR_calc(){
+    // Close the BMR calculator
+    public void close_BMR_calc() {
         System.out.println("\n----------------------------------------\n");
         System.out.print("Thank you for using the BMR Calculator! Press \"Enter\" to return to the main menu.");
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
+        this.scanner.nextLine();
+        this.scanner.nextLine();
     }
 
-    public static void clear(){
+    // Clear the console
+    public static void clear() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
-    //Displays BMR Calculator
-    public void display_bmr_calc(){
-        request_information();
+    // Display the BMR calculator
+    public void display_bmr_calc() {
+        getInfo();
         System.out.println("----------------------------------------\n");
-        System.out.println("You have a Basal Metabolic Rate (BMR) of " + this.BMR + " calories per day.\n"); 
+        System.out.println("You have a Basal Metabolic Rate (BMR) of " + this.BMR + " calories per day.\n");
         System.out.println("You should aim to consume " + this.goal_cal + " calories per day to reach your goal of " + this.goal_weight + " pounds.");
     }
 
-    //Requests user information
-    public void request_information(){
+    // Gather user information
+    public void getInfo() {
         File profile_file = new File("user_profile.txt");
-        String[] data = new String[4];
-        if(profile_file.exists()){
-            try(BufferedReader br = new BufferedReader(new FileReader("user_profile.txt"))){
-                String line;
-                while ((line = br.readLine()) != null) {
-                    if (line.startsWith("Age:")) {
-                        this.age = Integer.parseInt(line.split(":")[1].trim());
-                    } else if (line.startsWith("Weight:")) {
-                        this.weight = Integer.parseInt(line.split(":")[1].trim());
-                    } else if (line.startsWith("Height:")) {
-                        this.height = Integer.parseInt(line.split(":")[1].trim());
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println("An error occurred while reading the file.");
-            }
-            Scanner scanner = new Scanner(System.in);
 
+        if (profile_file.exists()) {
+            loadProfile(profile_file);
+        } else {
             clear();
-
-            System.out.println("----------------------------------------\n");
-
-            //SET SEX
-            while(true){
-                try{
-                    System.out.println("Please enter your sex...");
-                    System.out.print("\n\tEnter (1) for Female and (2) for Male: ");
-                    this.sex = scanner.nextInt();
-
-                    //CHECK IF INPUT IS VALID
-                    if (this.sex == 1 || this.sex == 2){break;}
-                    else{System.out.println("\n\tInvalid input. Please enter (1) for Female or (2) for Male.\n");}
-                } catch(InputMismatchException e){
-                    System.out.println("\n\tInvalid input. Please enter a valid integer for sex.\n");
-                    scanner.nextLine();
-                }
-            }
-            clear();
-
-            System.out.println("----------------------------------------\n");
-
-            System.out.println("Please enter the number for the category that best fits your lifestyle...");
-            System.out.println("\n\t(1) Sedentary (2) Lightly Active (3) Moderately Active (4) Very Active (5) Super Active");
-            
-            //SET LIFESTYLE
-            while(true){
-                try{
-                    System.out.print("\nEnter your choice: ");
-                    this.activity_level = scanner.nextInt();
-                    
-                    //CHECK IF INPUT IS VALID
-                    if(this.activity_level >= 1 && this.activity_level <= 5){break;}
-                    else{System.out.println("\nInvalid input. Please enter a valid integer between 1 and 5.");}
-                } catch(InputMismatchException e){
-                    System.out.println("\nInvalid input. Please enter a valid integer for lifestyle category.");
-                    scanner.nextLine();
-                }
-            }
-        }
-        else{
-            clear();
-
             System.out.println("Please enter the following information to calculate your BMR... ");
-
-            Scanner scanner = new Scanner(System.in);
-
-            //SET AGE
-            while(true){
-                try{
-                    System.out.print("\n\tAge: ");
-                    this.age = scanner.nextInt();
-                    break;
-                } catch(InputMismatchException e){
-                    System.out.println("\n\tInvalid input. Please enter a valid integer for age.");
-                    scanner.nextLine();
-                }
-            }
-
-            //SET WEIGHT
-            while(true){
-                try{
-                    System.out.print("\tWeight (in pounds): ");
-            this.weight = scanner.nextInt();
-                    break;
-                } catch(InputMismatchException e){
-                    System.out.println("\n\tInvalid input. Please enter a valid integer for weight.\n");
-                    scanner.nextLine();
-                }
-            }
-
-            //SET HEIGHT
-            while(true){
-                try{
-                    System.out.print("\tHeight (in inches): ");
-                    this.height = scanner.nextInt();
-                    break;
-                } catch(InputMismatchException e){
-                    System.out.println("\n\tInvalid input. Please enter a valid integer for height.\n");
-                    scanner.nextLine();
-                }
-            }
-
-
-            clear();
-
-            //SET SEX
-            while(true){
-                try{
-                    System.out.println("Please enter your sex...");
-                    System.out.print("\n\tEnter (1) for Female and (2) for Male: ");
-                    this.sex = scanner.nextInt();
-
-                    //CHECK IF INPUT IS VALID
-                    if (this.sex == 1 || this.sex == 2){break;}
-                    else{System.out.println("\n\tInvalid input. Please enter (1) for Female or (2) for Male.\n");}
-                } catch(InputMismatchException e){
-                    System.out.println("\n\tInvalid input. Please enter a valid integer for sex.\n");
-                    scanner.nextLine();
-                }
-            }
-
-            clear();
-
-            System.out.println("Please enter the number for the category that best fits your lifestyle...");
-            System.out.println("\n\t(1) Sedentary \n\t(2) Lightly Active \n\t(3) Moderately Active \n\t(4) Very Active \n\t(5) Super Active");
- 
-            //SET LIFESTYLE
-            while(true){
-                try{
-                    System.out.print("\nEnter your choice: ");
-                    this.activity_level = scanner.nextInt();
-                    
-                    //CHECK IF INPUT IS VALID
-                    if(this.activity_level >= 1 && this.activity_level <= 5){break;}
-                    else{System.out.println("\nInvalid input. Please enter a valid integer between 1 and 5.");}
-                } catch(InputMismatchException e){
-                    System.out.println("\nInvalid input. Please enter a valid integer for lifestyle category.");
-                    scanner.nextLine();
-                }
-            }
-            
+            this.age = getInput("Age: ");
+            this.weight = getInput("Weight (in pounds): ");
+            this.height = getInput("Height (in inches): ");
         }
+
+        this.sex = getInput("Enter (1) for Female and (2) for Male: ", 1, 2);
+        this.activity_level = getInput("Enter your lifestyle choice: \n\t(1) Sedentary \n\t(2) Lightly Active \n\t(3) Moderately Active \n\t(4) Very Active \n\t(5) Super Active", 1, 5);
 
         File goal_file = new File("goals.txt");
-        if(goal_file.exists()){
-            try(BufferedReader br = new BufferedReader(new FileReader("goals.txt"))){
-                String line;
-                while ((line = br.readLine()) != null) {
-                    if (line.startsWith("Goal Weight (in pounds):")) {
-                        this.goal_weight = Integer.parseInt(line.split(":")[1].trim());
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println("An error occurred while reading the file.");
-            }
-        }
-        else{
-            Scanner scanner = new Scanner(System.in);
-
-            while(true){
-                try{
-                    System.out.print("\nGoal weight (in pounds): ");
-                    this.goal_weight = scanner.nextInt();
-                    break;
-                } catch(InputMismatchException e){
-                    System.out.println("\nInvalid input. Please enter a valid integer for goal weight.");
-                    scanner.nextLine();
-                }
-            }
+        if (goal_file.exists()) {
+            loadGoal(goal_file);
+        } else {
+            this.goal_weight = getInput("Goal weight (in pounds): ");
         }
 
-        calculate_bmr();
+        calculateBMR();
     }
 
-    //Calculates BMR and goal calories
-    public int calculate_bmr(){
-        clear();
-        if(this.sex == 2){
-            this.BMR = (int) (66 + (6.23 * this.weight) + (12.7 * this.height) - (6.8 * this.age));
+    // Load profile information from a file
+    private void loadProfile(File profile_file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(profile_file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("Age:")) {
+                    this.age = Integer.parseInt(line.split(":")[1].trim());
+                } else if (line.startsWith("Weight:")) {
+                    this.weight = Integer.parseInt(line.split(":")[1].trim());
+                } else if (line.startsWith("Height:")) {
+                    this.height = Integer.parseInt(line.split(":")[1].trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        else{
+    }
+
+    // Load goal information from a file
+    private void loadGoal(File goal_file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(goal_file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("Goal Weight (in pounds):")) {
+                    this.goal_weight = Integer.parseInt(line.split(":")[1].trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Get input from the user
+    private int getInput(String prompt) {
+        while (true) {
+            try {
+                System.out.print(prompt);
+                return this.scanner.nextInt();
+            } catch (InputMismatchException e) {
+                e.printStackTrace();
+                this.scanner.nextLine();
+            }
+        }
+    }
+
+    // Get input from the user within a specific range
+    private int getInput(String prompt, int min, int max) {
+        while (true) {
+            try {
+                System.out.print(prompt);
+                int value = this.scanner.nextInt();
+                if (value >= min && value <= max) {
+                    return value;
+                } else {
+                    System.out.println("Invalid input. Please enter a valid integer between " + min + " and " + max + ".");
+                }
+            } catch (InputMismatchException e) {
+                e.printStackTrace();
+                this.scanner.nextLine();
+            }
+        }
+    }
+
+    // Calculate BMR and set goal calories
+    public int calculateBMR() {
+        clear();
+        if (this.sex == 2) {
+            this.BMR = (int) (66 + (6.23 * this.weight) + (12.7 * this.height) - (6.8 * this.age));
+        } else {
             this.BMR = (int) (655 + (4.35 * this.weight) + (4.7 * this.height) - (4.7 * this.age));
         }
-        switch(this.activity_level){
+        switch (this.activity_level) {
             case 1:
                 this.goal_cal = (int) (this.BMR * 1.2);
                 break;
@@ -252,32 +166,29 @@ public class BMR_Calc {
                 this.goal_cal = (int) (this.BMR * 1.9);
                 break;
         }
-        int weight_difference = this.weight - this.goal_weight;
-        if(weight_difference <= -40){
-            this.goal_cal *= 1.4;
-        }
-        else if(weight_difference <= -20 && weight_difference > -40){
-            this.goal_cal *= 1.3;
-        }
-        else if(weight_difference <= -10 && weight_difference > -20){
-            this.goal_cal *= 1.2;
-        }
-        else if(weight_difference < 0 && weight_difference > -10){
-            this.goal_cal *= 1.1;
-        }
-        else if(weight_difference >= 40){
-            this.goal_cal *= 0.6;
-        }
-        else if(weight_difference >= 20 && weight_difference < 40){
-            this.goal_cal *= 0.7;
-        }
-        else if(weight_difference >= 10 && weight_difference < 20){
-            this.goal_cal *= 0.8;
-        }
-        else if(weight_difference > 0 && weight_difference < 10){
-            this.goal_cal *= 0.9;
-        }
+        adjustCalories();
         return BMR;
     }
 
+    // Adjust goal calories based on the weight difference
+    private void adjustCalories() {
+        int weight_difference = this.weight - this.goal_weight;
+        if (weight_difference <= -40) {
+            this.goal_cal *= 1.4;
+        } else if (weight_difference <= -20 && weight_difference > -40) {
+            this.goal_cal *= 1.3;
+        } else if (weight_difference <= -10 && weight_difference > -20) {
+            this.goal_cal *= 1.2;
+        } else if (weight_difference < 0 && weight_difference > -10) {
+            this.goal_cal *= 1.1;
+        } else if (weight_difference >= 40) {
+            this.goal_cal *= 0.6;
+        } else if (weight_difference >= 20 && weight_difference < 40) {
+            this.goal_cal *= 0.7;
+        } else if (weight_difference >= 10 && weight_difference < 20) {
+            this.goal_cal *= 0.8;
+        } else if (weight_difference > 0 && weight_difference < 10) {
+            this.goal_cal *= 0.9;
+        }
+    }
 }
